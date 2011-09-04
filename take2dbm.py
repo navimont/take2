888614f54bd3ad0983b838c0916df2b637bf0bff
Stefan W <sw1@monton.de>
2011-09-03 23:31:09 -0400

    JSON/Yaml export works

--- take2dbm.py
+++ take2dbm.py
@@ -1,22 +1,80 @@
 """Take2 Database scheme"""
 
-import geo.geomodel
 from google.appengine.ext import db
 from google.appengine.ext.db import polymodel
+from google.appengine.ext.db import BadValueError
 
-class Contact(polymodel.PolyModel)
+class FuzzyDate(object):
+    """A date which allows unknown day/month or year left out
+    from: http://code.google.com/appengine/articles/extending_models.html
+    """
+    def __init__(self, year=0, month=0, day=0):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def has_day(self):
+        return self.day > 0
+
+    def has_month(self):
+        return self.month > 0
+
+    def has_year(self):
+        return self.year > 0
+
+    def __str__(self):
+        return '%02d/%02d/%04d' % (self.day,self.month,self.year)
+
+    def __not__(self):
+        return (not(self.has_year() or
+                self.has_month() or
+                self.has_day()))
+
+class FuzzyDateProperty(db.Property):
+
+    # Tell what the user type is.
+    data_type = FuzzyDate
+
+    # For writing to datastore.
+    def get_value_for_datastore(self, model_instance):
+        date = super(FuzzyDateProperty,self).get_value_for_datastore(model_instance)
+        return (date.month * 1000000) + (date.day * 10000) + date.year
+
+    # For reading from datastore.
+    def make_value_from_datastore(self, value):
+        if value is None:
+            return None
+        return FuzzyDate(month=value / 1000000,
+                         day=(value / 10000) % 100,
+                         year=value % 10000)
+
+    def validate(self, value):
+        if value is not None and not isinstance(value, FuzzyDate):
+            raise BadValueError('Property %s must be convertible '
+                                'to a FuzzyDate instance (%s)' %
+                                (self.name, value))
+        return super(FuzzyDateProperty, self).validate(value)
+
+    def default_value(self):
+        return FuzzyDate(0,0,0)
+
+    def empty(self, value):
+        return not value
+
+
+class Contact(polymodel.PolyModel):
     """Base class for person and Company"""
     # person's first name or name of a company
     name = db.StringProperty(required=True)
 
-class Company(Contact)
+class Company(Contact):
     """Represents a company"""
 
 class Person(Contact):
     """A natural Person"""
     nickname = db.StringProperty()
     lastname = db.StringProperty()
-    birthday = db.DateProperty()
+    birthday = FuzzyDateProperty()
     # a photo
     photo = db.BlobProperty()
     # where am I (right now)
@@ -24,25 +82,26 @@ class Person(Contact):
     # google login
     user = db.UserProperty()
 
-class Country(db.Model)
+class Country(db.Model):
     """Countries of the World"""
     name = db.StringProperty()
 
-class Take2(polymodel.PolyModel)
+class Take2(polymodel.PolyModel):
     """Base class for a contac's properties.
     contact refers to the person/company
     """
     # Reference to a Person or a Company
     contact = db.ReferenceProperty(Contact, required=True)
     # creation timestamp
-    timestamp = db.DateTimeProperty(required=True)
+    timestamp = db.DateTimeProperty(auto_now=True)
     # how to treat this connection
-    attic = db.StringProperty(required=True, choices=["Open",
+    take2 = db.StringProperty(required=True, default="Restricted",
+                                      choices=["Open",
                                        "Restricted",
                                        "Private",
                                        "Archived"])
 
-class Link(Take2)
+class Link(Take2):
     """Links between Persons/Contacts
     This elements exists always twice for every link
     such as Bob can have another link to Alice as vice versa
@@ -62,22 +121,23 @@ class Address(Take2):
     landline_phone = db.PhoneNumberProperty()
     country = db.ReferenceProperty(Country)
 
-class Mobile(Take2)
+class Mobile(Take2):
     # mobile number
     mobile = db.PhoneNumberProperty(required=True)
 
-class Web(Take2)
+class Web(Take2):
     # web address
     web = db.LinkProperty(required=True)
 
-class Email(Take2)
+class Email(Take2):
     # email
-    email = emailProperty()
+    email = db.EmailProperty()
 
-class Note(Take2)
+class Note(Take2):
     # note
     note = db.TextProperty()
 
-class Other(Take2)
+class Other(Take2):
     """Any other information"""
-    type = db.StringProperty(choices=)
+    what = db.StringProperty(choices=[])
+    text = db.StringProperty()
--- take2export.py
+++ take2export.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+import yaml
 import json
 from google.appengine.ext import db
 from google.appengine.api import users
@@ -9,180 +10,92 @@ from google.appengine.ext import webapp
 from google.appengine.ext.db import Key
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
-from epoidbm import Epoicon, Osmtag
+from take2dbm import Contact, Person, Company, Take2, FuzzyDate
+from take2dbm import Link, Email, Address, Mobile, Web, Note, Other
 
-
-class LoginPage(webapp.RequestHandler):
-    def get(self):
-        self.redirect(users.create_login_url('index.html'))
-
-class LogoutPage(webapp.RequestHandler):
-    def get(self):
-        self.redirect(users.create_logout_url('index.html'))
-
-class EpoiAdminPage(webapp.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-
-        # not logged in
-        if not user:
-            self.redirect(users.create_login_url(self.request.uri))
-            return
-
-        template_values = {'nickname': user.nickname()}
-
-        # not an administrator
-        if not users.is_current_user_admin():
-            path = os.path.join(os.path.dirname(__file__), 'sorrynoadmin.html')
-            self.response.out.write(template.render(path, template_values))
-            return
-
-        # find all icons in icondir directory
-        # note that icondir is only used to import new icon files
-        # into the storage. The files will later be served from
-        # the storage, not from icondir!
-        iconfiles = os.listdir(icondir)
-        # keep only those with ending *.png
-        iconfiles = [ic for ic in iconfiles if ic.endswith('.png') ]
-        logging.debug ("%3d     iconfiles found: %s" % (len(iconfiles),[", ".join(iconfiles)]))
-
-        # find all existing epoi icons in the storage
-        dbicons = Epoicon.all()
-        dbiconfiles = [dbi.file for dbi in dbicons]
-        logging.debug ("%3d icons in storage" % (len(dbiconfiles)))
-
-        #
-        # Those icons which are in the file list but not in the storage
-        # have to be added to the storage
-        #
-        new_icons = set(iconfiles).difference(set(dbiconfiles))
-        logging.debug ("%3d new iconfiles found: %s" % (len(new_icons),[", ".join(new_icons)]))
-        imported_icons = []
-        for ni in new_icons:
-            try:
-                fp = open(os.path.join(icondir,ni), 'rb')
-                try:
-                    icondata = fp.read()
-                finally:
-                    fp.close()
-                imported_icons.append(ni)
-            except IOError:
-                logging.critical("Could not read file %s/%s" % (icondir,ni))
-            # The name has to be adjusted by the user. For now use
-            # the upper case filename without ending
-            dbi = Epoicon(name=ni[:-4].upper(), file=ni, icon=icondata)
-            dbi.put()
-        logging.debug ("%3d iconfiles imported:  %s" % (len(imported_icons),[",".join(imported_icons)]))
-
-        icons = []
-        for epi in Epoicon.all():
-            icons.append({'file': os.path.join('icon',epi.file),
-                          'id':str(epi.key()),
-                          'name':epi.name})
-
-        template_values['icons'] = icons
-
-        # render administration page
-        path = os.path.join(os.path.dirname(__file__), 'epoiadmin.html')
-        self.response.out.write(template.render(path, template_values))
 
 class Take2Export(webapp.RequestHandler):
     """Export the relation between icons and osm tags (backup)"""
 
-    def encodeTake2(obj_class, contact, archived = False)
-        """Encodes objects derived from Take2 class
-        into a python data structure.
-        q_object is a query"""
-
+    def encodeTake2(self, obj_class, contact, archived=False):
+        """Encodes obj_class into a python data structure.
+        obj_class must be a descendant of Take2 class.
+        """
         q_obj = obj_class.all()
         q_obj.filter("contact =", contact.key())
+        q_obj.order('-timestamp')
+        results = []
+        for obj in q_obj:
+            res = {}
+            # archive only the latest unless archived=True
+            if len(results) == 0 or archived:
+                res['key'] = str(obj.key())
+                res['type'] = obj.class_name().lower()
+                res['timestamp'] = "%04d-%02d-%02d %02d:%02d:%02d.%06d" % (obj.timestamp.year,
+                                                                          obj.timestamp.month,
+                                                                          obj.timestamp.day,
+                                                                          obj.timestamp.hour,
+                                                                          obj.timestamp.minute,
+                                                                          obj.timestamp.second,
+                                                                          obj.timestamp.microsecond)
+                res['take2'] = obj.take2
+                if obj.class_name() == "Email":
+                    res['email'] = obj.email
+                elif obj.class_name() == "Web":
+                    res['web'] = obj.web
+                elif obj.class_name() == "Link":
+                    res['link'] = obj.link
+                    res['link_to'] = str(obj.link_to.key())
+                elif obj.class_name() == "Address":
+                    res['location_lat'] = obj.location.lat
+                    res['location_lon'] = obj.location.lon
+                    res['adr'] = obj.adr
+                    res['landline_phone'] = obj.landline_phone
+                    res['country'] = obj.country
+                elif obj.class_name() == "Mobile":
+                    res['mobile'] = obj.mobile
+                elif obj.class_name() == "Note":
+                    res['note'] = obj.note
+                elif obj.class_name() == "Other":
+                    res['what'] = obj.what
+                    res['text'] = obj.text
+                else:
+                    assert True, "Invalid class name: %s" % obj.class_name()
 
-        for obj in q_object:
-            if
+            results.append(res)
+        return results
 
-    def encodeContact(self, contact, archived=False)
+
+    def encodeContact(self, contact, archive=False):
         """Encodes Contact data for export and returns a
         python data structure of dictionaries and lists.
         If archived=True, data will include the
         complete history and also archived data.
         """
+        logging.debug("export name: %s" % (contact.name))
         res = {}
         res['key'] = str(contact.key())
         res['name'] = contact.name
-        if contact.class_name == "Person":
-            res['type'] = "person"
+        res['type'] = contact.class_name().lower()
+        if contact.class_name() == "Person":
             # google account
-            res['user'] = user.nickname()
+            res['user'] = contact.user
             # personal nickname
             res['nickname'] = contact.nickname
             res['lastname'] = contact.lastname
-            res['birthday'] = contact.birthday
-        elif contact.class_name == "Company":
-            res['type'] = "company"
+            res['birthday'] = "%04d-%02d-%02d" % (contact.birthday.year,contact.birthday.month,contact.birthday.day)
+        elif contact.class_name() == "Company":
+            # nothing to do
+            pass
+        else:
+            assert True, "Invalid class name: %s" % contact.class_name()
 
-
-        encodeTake2(, archive)
-
-
-        res['links'] = []
-        for link in contact.link_set:
-            ln = {}
-            ln['timestamp'] = link.timestamp
-            ln['take2'] = link.take2
-            ln['link'] = link.link
-            ln['link_to'] = link_to.link_to
-            res['links'].append(ln)
-
-        res['addresses'] = []
-        for address in contact.address_set:
-            addr = {}
-            addr['location'] = address.location
-            addr['adr'] = address.adr
-            addr['landline_phone'] = address.landline_phone
-            addr['country'] = address.country
-            addr['timestamp'] = addr.timestamp
-            addr['take2'] = addr.take2
-            res['addresses'].append(addr)
-
-        res['mobiles'] = []
-        for mobile in contact.mobile_set:
-            mobi = {}
-            mobi['timestamp'] = mobile.timestamp
-            mobi['take2'] = mobile.take2
-            mobi['mobile'] = mobile.mobile
-            res['mobiles'].append(mobi)
-
-        res['webs'] = []
-        for web in contact.web_set:
-            wb = {}
-            wb['timestamp'] = web.timestamp
-            wb['take2'] = web.take2
-            wb['web'] = web.web
-            res['webs'].append(wb)
-
-        res['emails'] = []
-        for email in contact.email_set:
-            em = {}
-            em['timestamp'] = email.timestamp
-            em['take2'] = email.take2
-            em['email'] = email.email
-            res['emails'].append(em)
-
-        res['notes'] = []
-        for note in contact.note_set:
-            nt = {}
-            nt['timestamp'] = note.timestamp
-            nt['take2'] = note.take2
-            nt['web'] = note.note
-            res['notes'].append(nt)
-
-        res['others'] = []
-        for other in contact.other_set:
-            ot = {}
-            ot['timestamp'] = other.timestamp
-            ot['take2'] = other.take2
-            ot['web'] = other.note
-            res['others'].append(ot)
+        # now encode the contact's address, email etc.
+        for obj in [Email,Note,Web,Address,Mobile,Link,Other]:
+            objs = self.encodeTake2(obj, contact, archive)
+            logging.debug("%s" % ", ".join(dir(obj)))
+            if objs:
+                res[obj.class_name().lower()] = objs
 
         return res
 
@@ -196,31 +109,32 @@ class Take2Export(webapp.RequestHandler):
             self.redirect(users.create_login_url(self.request.uri))
             return
 
-        template_values = {'nickname': user.nickname()}
-
         if self.request.get('archive',"") == 'True':
             archive = True
         else:
             archive = False
 
-        self.response.headers['Content-Type'] = 'application/json'
+        logging.info("export format:  archive: %d user: %s admin: %d" % (archive,user.nickname(),users.is_current_user_admin()))
+        self.response.headers['Content-Type'] = 'text/plain'
 
         # Administrator exports everything
+        contacts = []
         if users.is_current_user_admin():
             q_con = Contact.all()
-
+            for con in q_con:
+                contacts.append(self.encodeContact(con, archive))
         else:
             q_us = Contact.all()
             q_us.filter("user =", user)
             us = q_us.fetch(1)
             if len(us) != 1:
-                logging.Critical("Found wrong # of user: %s [%s]" % (user.nickname(),
-                    ", ".join[u.name for u in us])
+                logging.Critical("Found wrong # of user: %s [%s]" % (user.nickname(), ", ".join([u.name for u in us])))
                 self.error(500)
                 return
-            else:
-            logging.info("export user: %s name: " % (user.nickname(), us[0].)
-            self.response.out.write(encodeContact(self, us, archived=archive))
+            contacts.append(self.encodeContact(us, False))
+
+        self.response.out.write(json.dumps(contacts,indent=2))
+        self.response.out.write(yaml.dump(contacts))
 
 
 class Take2Import(webapp.RequestHandler):
@@ -272,15 +186,33 @@ class Take2Import(webapp.RequestHandler):
         fp.close()
         self.redirect('/epoiadmin')
 
+def example():
+    for c in Contact.all():
+        c.delete()
+    eso = Company (name = 'ESO')
+    eso.put()
+    stef = Person(name = u'Stphane',
+                  nickname = 'Stef',
+                  lastname = 'Wehner',
+                  birthday = FuzzyDate(year=0,month=6,day=15))
+    stef.put()
+    email = Email(contact=stef,email='sw1@monton.de')
+    email.put()
+    dirk = Person(name='Dirk')
+    dirk.put()
+    link = Link(contact=stef,link="Friend",link_to=dirk)
+    link.put()
+    adr = Address(contact=stef,adr=['104 Adelphi','Brooklyn','NY','11205'],location=db.GeoPt(-70.1,30.0))
+    adr.put()
+    mobile = Mobile(contact=stef,mobile='616-204-7136')
+    mobile.put()
 
-application = webapp.WSGIApplication([('/epoiadmin/icon.*', EpoiAdminEditIconPage),
-                                      ('/epoiadmin', EpoiAdminPage),
-                                      ('/take2/import.*', EpoiAdminImportIcons),
-                                      ('/take2/export', EpoiAdminExportIcons),
-                                      ('/login', LoginPage),
-                                      ('/logout', LogoutPage)],debug=True)
+application = webapp.WSGIApplication([('/import.*', Take2Import),
+                                      ('/export', Take2Export),
+                                     ],debug=True)
 
 def main():
+    example()
     logging.getLogger().setLevel(logging.DEBUG)
     run_wsgi_app(application)
 
